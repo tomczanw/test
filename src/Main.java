0f52319d452e8b16a060030da79c910d92851db2
Tomasz Wrobel <tomek618@o2.pl>
2019-05-20 14:33:24 +0200

    some changes

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        User user = new User("Tomasz", "blabla ");
+
+        User user1 = new User();
+
+
+        LocalDate localDate = LocalDate.now();
+
+
+    }
+}
